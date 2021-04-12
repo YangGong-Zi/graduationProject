6f4ed3e124c9efaaf0ab3f514fc3a5f49adb7b36
yanggongzi <1758417159@qq.com>
2021-04-12 23:12:36 +0800

    3

--- IM_analyze/test.py
+++ IM_analyze/test.py
@@ -1,1 +1,17 @@
-print('中文')
+'''
+Author: 在下杨公子
+Date: 2021-04-12 21:09:21
+LastEditTime: 2021-04-12 22:05:30
+FilePath: \graduationProject\IM_analyze\test.py
+'''
+# print('中文');
+from bs4 import BeautifulSoup
+import requests
+def getdata():
+    res = requests.get(url,headers = "mingker.cn")
+    soup = BeautifulSoup(res.content,'lxml')
+    return soup
+def main():
+    getdata()
+if __name__ == '__main__':
+    main()
